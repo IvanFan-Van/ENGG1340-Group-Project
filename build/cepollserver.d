b8975318afb698ec32f30e8523499e94f7b82346
IvanFan <3323730286@qq.com>
2024-04-15 21:06:10 +0800

    添加头文件

---
+++ build/cepollserver.d
@@ -0,0 +1,10 @@
+build/cepollserver.o: src/server/cepollserver.cpp \
+ include/server/cepollserver.h include/battleship/action.h \
+ include/battleship/constants.h include/battleship/board.h \
+ include/battleship/point.h include/battleship/constants.h
+include/server/cepollserver.h:
+include/battleship/action.h:
+include/battleship/constants.h:
+include/battleship/board.h:
+include/battleship/point.h:
+include/battleship/constants.h:
---
+++ include/battleship/game.h
@@ -0,0 +1,56 @@
+#ifndef BATTLESHIP_GAME_H
+#define BATTLESHIP_GAME_H
+
+#include "board.h"
+#include <string>
+
+using namespace std;
+
+class Game
+{
+private:
+  Board playerBoard;   // The board representing the player's ships and attacks.
+  Board computerBoard; // The board representing the computer's ships and attacks.
+  bool playerTurn;     // A flag indicating whether it is the player's turn.
+
+  /**
+   * @brief Places the ships on the given board.
+   *
+   * This function is used to place the ships on the board for either the player or the computer.
+   *
+   * @param board The board on which to place the ships.
+   * @param isPlayer A flag indicating whether the board belongs to the player.
+   */
+  void placeShips(Board &board, bool isPlayer);
+
+  /**
+   * @brief Executes the player's move.
+   *
+   * This function is called when it is the player's turn to make a move.
+   */
+  void playerMove();
+
+  /**
+   * @brief Executes the computer's move.
+   *
+   * This function is called when it is the computer's turn to make a move.
+   */
+  void computerMove();
+
+public:
+  /**
+   * @brief Default constructor for the Game class.
+   */
+  Game();
+
+  ~Game();
+
+  /**
+   * @brief Starts the game.
+   *
+   * This function starts the game by initializing the boards, placing the ships, and executing the turns until the game is over.
+   */
+  void start();
+};
+
+#endif // BATTLESHIP_GAME_H
