5d3507ad55559415ce6a8ac4397706bebb1073c2
RyanWang123456 <u3622342@connect.hku.hk>
2024-04-17 23:13:17 +0800

    add constant.h

---
+++ include/battleship/constants.h
@@ -0,0 +1,11 @@
+#ifndef CONSTANS_H
+#define CONSTANS_H
+
+const int BOARD_SIZE = 10;
+const int SHIPS[] = {4, 3, 3, 2, 2, 2, 1, 1, 1, 1};
+extern const char EMPTY;
+extern const char SHIP;
+extern const char HIT;
+extern const char MISS;
+
+#endif
