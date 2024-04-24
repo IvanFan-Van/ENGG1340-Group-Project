bbce70b28359074da49468c5167c79b165126594
IvanFan <3323730286@qq.com>
2024-04-24 11:52:59 +0800

    给棋子添加颜色

--- include/battleship/constants.h
+++ include/battleship/constants.h
@@ -1,12 +1,20 @@
 #ifndef CONSTANTS_H
 #define CONSTANTS_H
 
+#include "battleship/color.h"
+#include <string>
+
 const int BOARD_SIZE = 10;
-const int SHIPS[] = {4, 3, 3, 2, 2, 2, 1, 1, 1, 1};
-// const int SHIPS[] = {1};
+// const int SHIPS[] = {4, 3, 3, 2, 2, 2, 1, 1, 1, 1};
+const int SHIPS[] = {1};
 extern const char EMPTY;
 extern const char SHIP;
 extern const char HIT;
 extern const char MISS;
+
+extern const std::string EMPTY_CELL;
+extern const std::string SHIP_CELL;
+extern const std::string HIT_CELL;
+extern const std::string MISS_CELL;
 
 #endif // CONSTANTS_H
