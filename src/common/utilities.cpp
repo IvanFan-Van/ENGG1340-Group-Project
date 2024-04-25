5729efcaac5a77ed3b2afebb50e333aab3df12ca
Menny <nindie624@gmail.com>
2024-04-25 09:50:49 +0800

    Bug-fix: target to be hit shown on my own board

--- src/common/utilities.cpp
+++ src/common/utilities.cpp
@@ -7,23 +7,23 @@ using namespace std;
 // New function to display both boards side by side
 void displayBoardsSideBySide(const Board &playerBoard,
                              Board &opponentBoard, bool showPlayerShips, int x, int y,
-                             bool showOpponentShips = false) {
+                             bool showOpponentShips) {
   clearScreen();
   cout << "  0 1 2 3 4 5 6 7 8 9      0 1 2 3 4 5 6 7 8 9\n";
   for (int i = 0; i < BOARD_SIZE; ++i) {
     // Display player's board row
     cout << char('A' + i) << ' ';
-    if (x == i){
-      playerBoard.displayRow(i, showPlayerShips, y);
-    }
-    else{
-      playerBoard.displayRow(i, showPlayerShips, -1);
-    }
+    playerBoard.displayRow(i, showPlayerShips, -1);
     cout << "   "; // Space between boards
 
     // Display computer's board row
     cout << char('A' + i) << ' ';
-    opponentBoard.displayRow(i, showOpponentShips, -1);
+    if (x == i){
+      opponentBoard.displayRow(i, showPlayerShips, y);
+    }
+    else{
+      opponentBoard.displayRow(i, showPlayerShips, -1);
+    }
     cout << endl;
   }
 }
