7c0ed75500977f41d1ae151dcf00006d0a6da536
john369 <john369@connect.hku.hk>
2024-04-27 22:51:47 +0800

    优化LOGO动画，添加菜单选项

--- src/common/utilities.cpp
+++ src/common/utilities.cpp
@@ -1,12 +1,12 @@
-#include "battleship/board.h"
 #include "common/utilities.h"
+#include "battleship/board.h"
 #include <iostream>
 
 using namespace std;
 
 // New function to display both boards side by side
-void displayBoardsSideBySide(const Board &playerBoard,
-                             Board &opponentBoard, bool showPlayerShips, int x, int y,
+void displayBoardsSideBySide(const Board &playerBoard, Board &opponentBoard,
+                             bool showPlayerShips, int x, int y,
                              bool showOpponentShips) {
   clearScreen();
   cout << "  0 1 2 3 4 5 6 7 8 9      0 1 2 3 4 5 6 7 8 9\n";
@@ -18,10 +18,9 @@ void displayBoardsSideBySide(const Board &playerBoard,
 
     // Display computer's board row
     cout << char('A' + i) << ' ';
-    if (x == i){
+    if (x == i) {
       opponentBoard.displayRow(i, false, y);
-    }
-    else{
+    } else {
       opponentBoard.displayRow(i, false, -1);
     }
     cout << endl;
@@ -35,4 +34,16 @@ void clearBoard() {
   for (int i = 0; i < 11; i++) {
     cout << "\033[2K\033[1A";
   }
+}
+
+// 获取居中打印字符串光标的位置
+int getCenteredPosition(const string &text, int terminalWidth) {
+  return (terminalWidth - text.length()) / 2;
+}
+
+// 居中打印字符串
+void printCentered(const string &text, int terminalWidth) {
+  int pos_x = getCenteredPosition(text, terminalWidth);
+  cout << "\033[" << pos_x << "C";
+  cout << text << endl;
 }
